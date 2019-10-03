93e372da603afa6e2510bc36aba05270581741ea
fetimo <tim@salocreative.co.uk>
2019-10-03 17:02:08 +0100

    Fixed test

--- src/icon.spec.js
+++ src/icon.spec.js
@@ -15,7 +15,7 @@ test('Render default Icon tag and styles', async () => {
   const svg = container.firstChild;
   const path = container.querySelector('path');
   // Assert
-  expect(svg).toHaveAttribute('style', 'width: 24px; height: 24px; margin: 0px;');
+  expect(svg).not.toHaveAttribute('style');
   expect(path).toHaveAttribute('d', 'M13,3V9H21V3M13,21H21V11H13M3,21H11V15H3M3,13H11V3H3V13Z');
   expect(path).toHaveAttribute('fill', '#00222b');
   expect(container.firstChild).toMatchSnapshot();
@@ -29,7 +29,7 @@ test('Render Icon with core props', async () => {
   const svg = container.firstChild;
   const path = container.querySelector('path');
   // Assert
-  expect(svg).toHaveAttribute('style', 'width: 100px; height: 100px; margin: 10px;');
+  expect(svg).not.toHaveAttribute('style');
   expect(path).toHaveAttribute('fill', '#fff');
   expect(path).toHaveAttribute('d', 'M20.71,7.04C21.1,6.65 21.1,6 20.71,5.63L18.37,3.29C18,2.9 17.35,2.9 16.96,3.29L15.12,5.12L18.87,8.87M3,17.25V21H6.75L17.81,9.93L14.06,6.18L3,17.25Z');
   expect(container.firstChild).toMatchSnapshot();
