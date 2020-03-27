168540bc89f372cfb41c8ac71eeb1243e34cf991
Tim Stone <tim@salocreative.co.uk>
2020-03-27 17:23:41 +0000

    Upgraded deps and added title

--- src/icon.js
+++ src/icon.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import { get } from 'lodash';
 
 import icons from './icons';
 
-const SVG = styled.svg`
+const SVG = styled.svg.attrs({
+  focusable: 'false'
+})`
   height: ${ ({ size }) => (typeof size === 'number' ? `${ size }px` : size) };
   width: ${ ({ size }) => (typeof size === 'number' ? `${ size }px` : size) };
   margin: ${ ({ margin }) => margin };
@@ -18,16 +19,24 @@ const Icon = ({
   icon,
   margin,
   size,
+  title,
+  standAlone,
   vAlign
 }) => (
   <SVG
     className={ className }
     margin={ margin }
     size={ size }
+    { ...(standAlone ? {
+      role: 'img'
+    } : {
+      'aria-hidden': 'true'
+    }) }
     vAlign={ vAlign }
     viewBox='0 0 24 24'
   >
-    <path fill={ fill } d={ get(icons, `${ icon }`, 'M13,3V9H21V3M13,21H21V11H13M3,21H11V15H3M3,13H11V3H3V13Z') } />
+    { title && <title>{ title }</title> }
+    <path fill={ fill } d={ icons?.[icon] || 'M13,3V9H21V3M13,21H21V11H13M3,21H11V15H3M3,13H11V3H3V13Z' } />
   </SVG>
 );
 
@@ -35,8 +44,10 @@ Icon.defaultProps = {
   className: null,
   fill: '#00222b',
   icon: 'dashboard',
+  standAlone: false,
   margin: '0',
   size: 24,
+  title: null,
   vAlign: null
 };
 
@@ -44,8 +55,10 @@ Icon.propTypes = {
   className: PropTypes.string,
   fill: PropTypes.string,
   icon: PropTypes.string,
+  standAlone: PropTypes.bool,
   margin: PropTypes.string,
   size: PropTypes.number,
+  title: PropTypes.string,
   vAlign: PropTypes.string
 };
 
