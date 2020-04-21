7bc8d65733f7507539b263b6a7a319b95a5dffa9
Tim Stone <tim@salocreative.co.uk>
2020-04-21 16:13:31 +0100

    Updated Storybook format

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -1,7 +1,6 @@
 /* eslint-disable import/no-extraneous-dependencies, import/extensions */
 
 import '@storybook/addon-links/register';
-import '@storybook/addon-options/register';
 import '@storybook/addon-knobs/register';
 import '@storybook/addon-jest/register';
 import '@storybook/addon-notes/register';
--- src/_icon.story.js
+++ src/_icon.story.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { storiesOf } from '@storybook/react';
 import { withTests } from '@storybook/addon-jest';
 import { withKnobs, text, number } from '@storybook/addon-knobs';
 import styled from 'styled-components';
-import { colours, P, Column, Row } from '@salo/core-ui';
+import {
+  colours, P, Column, Row
+} from '@salo/core-ui';
 
 // TESTS
 import results from '../.storybook/jest-test-results.json';
@@ -16,12 +17,6 @@ import icons from './icons';
 import README from './README.md';
 
 // Start of story logic
-const stories = storiesOf('Icon', module);
-stories.addDecorator(withKnobs);
-stories.addDecorator(withTests({ results }));
-stories.addParameters({ jest: ['icon'] });
-
-
 const iconKeys = Object.keys(icons);
 
 // STYLES
@@ -45,25 +40,38 @@ const StyledP = styled(P)`
 `;
 
 
-stories.add(
-  'Basic',
-  () => {
-    const fill = text('Fill', '#262729');
-    const size = number('Size', 24);
-    return (
-      <Row>
-        { iconKeys.map(val => {
-          return (
-            <Column tablet={ 6 } medium={ 4 } large={ 3 } key={ val }>
-              <Wrapper>
-                <Icon icon={ val } fill={ fill } size={ size } />
-                <StyledP>{ val }</StyledP>
-              </Wrapper>
-            </Column>
-          );
-        }) }
-      </Row>
-    );
-  },
-  { notes: README, info: { source: false, propTablesExclude: [StyledP, Wrapper, Column, Row] } }
-);
+export const Basic = () => {
+  const fill = text('Fill', '#262729');
+  const size = number('Size', 24);
+  return (
+    <Row>
+      { iconKeys.map(val => {
+        return (
+          <Column tablet={ 6 } medium={ 4 } large={ 3 } key={ val }>
+            <Wrapper>
+              <Icon icon={ val } fill={ fill } size={ size } />
+              <StyledP>{ val }</StyledP>
+            </Wrapper>
+          </Column>
+        );
+      }) }
+    </Row>
+  );
+};
+
+Basic.story = {
+  decorators: [
+    withKnobs,
+    withTests({
+      results
+    })
+  ],
+  parameters: {
+    notes: README,
+    jest: ['icon']
+  }
+};
+
+export default {
+  title: 'Icon'
+};
